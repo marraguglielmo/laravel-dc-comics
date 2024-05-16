efb528e7c87923fc44d3f9077c4eb97e3435a6c9
marraguglielmo <gugmarra@gmail.com>
2024-05-16 15:52:59 +0200

    [add] helper [fix] seeder

--- database/seeders/ComicsTableSeeder.php
+++ database/seeders/ComicsTableSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use App\Models\Comic;
+use App\Functions\Helper;
 
 class ComicsTableSeeder extends Seeder
 {
@@ -14,6 +15,19 @@ class ComicsTableSeeder extends Seeder
     public function run(): void
     {
         $comics = config('comics');
-        dd($comics);
+        foreach($comics as $comic){
+            $new_comic = new Comic();
+            $new_comic->title = $comic['title'];
+            $new_comic->slug = Helper::generateSlug($new_comic->title, new Comic());
+            $new_comic->description = $comic['description'];
+            $new_comic->thumb = $comic['thumb'];
+            $new_comic->price = $comic['price'];
+            $new_comic->series = $comic['series'];
+            $new_comic->sale_date = $comic['sale_date'];
+            $new_comic->type = $comic['type'];
+            $new_comic->artists = $comic['artists'];
+            $new_comic->writers = $comic['writers'];
+            dump($new_comic);
+        }
     }
 }
