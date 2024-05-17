a0d1945a60c0137fddc709c7dfc6b78041f16d34
marraguglielmo <gugmarra@gmail.com>
2024-05-17 15:26:18 +0200

    [add] fillable

--- app/Http/Controllers/ComicsController.php
+++ app/Http/Controllers/ComicsController.php
@@ -35,19 +35,13 @@ class ComicsController extends Controller
         $form_data = $request->all();
 
         $new_comic = new Comic();
-            $new_comic->title = $form_data['title'];
-            $new_comic->slug = Helper::generateSlug($new_comic->title, new Comic());
-            $new_comic->description = $form_data['description'];
-            $new_comic->thumb = $form_data['thumb'];
-            $new_comic->price = $form_data['price'];
-            $new_comic->series = $form_data['series'];
-            $new_comic->sale_date = $form_data['sale_date'];
-            $new_comic->type = $form_data['type'];
-            $new_comic->artists = $form_data['artists'] ;
-            $new_comic->writers = $form_data['writers'];
 
-            // dd($new_comic);
-            $new_comic->save();
+        // form_data non contiene lo slug, quindi lo aggiungo
+        $form_data['slug'] = Helper::generateSlug($form_data['titolo'], new Comic);
+
+        $new_comic->fill($form_data);
+
+        $new_comic->save();
 
         return redirect()->route('comics.show', $new_comic);
     }
