b70c8000da22dcd8d7ad56cdebb66d4396a9d6f3
Guglielmo Marra <156308467+marraguglielmo@users.noreply.github.com>
2024-05-20 15:42:31 +0200

    [add] request

--- app/Http/Controllers/ComicsController.php
+++ app/Http/Controllers/ComicsController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Comic;
 use App\Functions\Helper;
+use App\Http\Requests\ComicRequest;
 
 class ComicsController extends Controller
 {
@@ -28,47 +29,8 @@ class ComicsController extends Controller
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(ComicRequest $request) //la validazione avviene qui
     {
-
-        // validazione dei dati
-        $request->validate(
-            [
-                'title' => 'required|min:3|max:255',
-                'thumb' => 'required|min:20|max:65353',
-                'price' => 'required|min:2|max:10',
-                'series' => 'required|min:5|max:50',
-                'sale_date' => 'required',
-                'type' => 'required|min:4|max:30',
-                'artists' => 'required|min:2',
-                'writers' => 'required|min:2 ',
-            ],
-            [
-                'title.required' => 'Il titolo è un campo obbligatorio',
-                'title.min' => 'Il titolo deve contenere almeno :min caratteri',
-                'title.max' => 'Il titolo non può contenere più di :max caratteri',
-                'thumb.required' => 'La thumb è un campo obbligatorio',
-                'thumb.min' => 'La thumb deve contenere almeno :min caratteri',
-                'thumb.max' => 'La thumb non può contenere più di :max caratteri',
-                'price.required' => 'Il prezzo è un campo obbligatorio',
-                'price.min' => 'Il prezzo deve contenere almeno :min caratteri',
-                'price.max' => 'Il prezzo non può contenere più di :max caratteri',
-                'series.required' => 'La serie è un campo obbligatorio',
-                'series.min' => 'La serie deve contenere almeno :min caratteri',
-                'series.max' => 'La serie non può contenere più di :max caratteri',
-                'sale_date.required' => 'Il giorno di vendita è un campo obbligatorio',
-                'type.required' => 'Il tipo è un campo obbligatorio',
-                'type.min' => 'Il tipo deve contenere almeno :min caratteri',
-                'type.max' => 'Il tipo non può contenere più di :max caratteri',
-                'artists.required' => 'Artisti è un campo obbligatorio',
-                'artists.min' => 'Artisti deve contenere almeno :min caratteri',
-                'writers.required' => 'Scrittori è un campo obbligatorio',
-                'writers.min' => 'Scrittori deve contenere almeno :min caratteri',
-
-            ]
-        );
-
-
 
         //ricevo da create i datri del nuovo fumetto, li salva nel database e reindirizzo alla show con l'id del nuovo fumetto
         // prendo tutti i dati provenienti dal form e li salvo in un array associativo
---
+++ app/Http/Requests/ComicRequest.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class ComicRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'title' => 'required|min:3|max:255',
+            'thumb' => 'required|min:20|max:65353',
+            'price' => 'required|min:2|max:10',
+            'series' => 'required|min:5|max:50',
+            'sale_date' => 'required',
+            'type' => 'required|min:4|max:30',
+            'artists' => 'required|min:2',
+            'writers' => 'required|min:2 ',
+        ];
+    }
+
+    public function messages()
+    {
+        return [
+            'title.required' => 'Il titolo è un campo obbligatorio',
+            'title.min' => 'Il titolo deve contenere almeno :min caratteri',
+            'title.max' => 'Il titolo non può contenere più di :max caratteri',
+            'thumb.required' => 'La thumb è un campo obbligatorio',
+            'thumb.min' => 'La thumb deve contenere almeno :min caratteri',
+            'thumb.max' => 'La thumb non può contenere più di :max caratteri',
+            'price.required' => 'Il prezzo è un campo obbligatorio',
+            'price.min' => 'Il prezzo deve contenere almeno :min caratteri',
+            'price.max' => 'Il prezzo non può contenere più di :max caratteri',
+            'series.required' => 'La serie è un campo obbligatorio',
+            'series.min' => 'La serie deve contenere almeno :min caratteri',
+            'series.max' => 'La serie non può contenere più di :max caratteri',
+            'sale_date.required' => 'Il giorno di vendita è un campo obbligatorio',
+            'type.required' => 'Il tipo è un campo obbligatorio',
+            'type.min' => 'Il tipo deve contenere almeno :min caratteri',
+            'type.max' => 'Il tipo non può contenere più di :max caratteri',
+            'artists.required' => 'Artisti è un campo obbligatorio',
+            'artists.min' => 'Artisti deve contenere almeno :min caratteri',
+            'writers.required' => 'Scrittori è un campo obbligatorio',
+            'writers.min' => 'Scrittori deve contenere almeno :min caratteri',
+        ];
+    }
+}
